f03aae62814920d0a81334f17cee3a61e59ff609
https-aya <138739258+https-aya@users.noreply.github.com>
2024-07-05 15:01:26 +0900

    EnemyBaseに追加

--- PacManProject/Develop/Objects/Enemy/EnemyBase.cpp
+++ PacManProject/Develop/Objects/Enemy/EnemyBase.cpp
@@ -9,3 +9,33 @@ EnemyBase::EnemyBase()
 EnemyBase::~EnemyBase()
 {
 }
+
+void EnemyBase::Initialize()
+{
+
+}
+
+void EnemyBase::Update()
+{
+
+}
+
+void EnemyBase::Draw() const
+{
+
+}
+
+void EnemyBase::Finalize()
+{
+
+}
+
+void EnemyBase::Movement(float delta_second)
+{
+
+}
+
+void EnemyBase::AnimationControl(float delta_second)
+{
+
+}
--- PacManProject/Develop/Objects/Enemy/EnemyBase.h
+++ PacManProject/Develop/Objects/Enemy/EnemyBase.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 #include "../GameObject.h"
-#include "../../Utility/StageData.h"
+
 
 enum eEnemyType {
 	Akabe,
@@ -10,13 +10,47 @@ enum eEnemyType {
 	Guzuta,
 };
 
+enum eEnemyState 
+{
+	ATTACK,
+	PATOROL,
+	IZIKE,
+	ESCAPE,
+};
+
+
+
 class EnemyBase : public GameObject
 {
+private:
+	enum eEnemyDirectionState : unsigned char
+	{
+		UP,
+		RIGHT,
+		DOWN,
+		LEFT,
+	};
+
 protected:
 	std::vector<int> Animation;
 	std::vector<int> MoveAnimation;
+	eEnemyType enemy_type;
+	eEnemyState enemy_state;
+	eEnemyDirectionState now_direction;
+	eEnemyDirectionState next_direction;
+	int animation_count;
+	float animation_time;
 
 public:
 	EnemyBase();
 	virtual ~EnemyBase();
+
+	virtual void Initialize();
+	virtual void Update();
+	virtual void Draw() const;
+	virtual void Finalize();
+
+private:
+	virtual void Movement(float delta_second);
+	virtual void AnimationControl(float delta_second);
 };
